bfea2b43d00d6b7102810d788ae3bb8512c8f6b7
esmerquenn <esmereyvazov@gmail.com>
2024-06-11 01:14:36 +0400

    j

--- src/components/childrenComponentss/sectionCarts/CartsSection.jsx
+++ src/components/childrenComponentss/sectionCarts/CartsSection.jsx
@@ -17,7 +17,7 @@ function CartsSection({ t }) {
     <>
       <div className="py-4 md:py-11 px-4 md:px-10 lg:px-20 bg-[#e6e7e8]">
         <Headings head={t("products")} />
-        <div className=" grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-9">
+        <div className=" grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-9">
           {products.map((product) => (
             <Cards key={product.id} id={product.id} name={product.name} t={t} />
           ))}
